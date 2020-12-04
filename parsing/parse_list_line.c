6c4deb4ec72bce23be050358b0552f0a861aca0d
Elhassane Lahyani <elahyani@e2r8p5.1337.ma>
2020-12-04 13:13:41 +0100

    Norm the parsing_list_line file

--- parsing/parse_list_line.c
+++ parsing/parse_list_line.c
@@ -6,11 +6,40 @@
 /*   By: elahyani <elahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 09:43:31 by elahyani          #+#    #+#             */
-/*   Updated: 2020/12/02 11:42:40 by elahyani         ###   ########.fr       */
+/*   Updated: 2020/12/04 11:00:15 by elahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	parse_pipe(char **line, t_cmd_list **hd, int *i)
+{
+	(!(*hd)->prev || ((*hd)->prev && (*hd)->prev->end)) ? (*hd)->start = 1 : 0;
+	(*line)[*i] = '\0';
+	if ((*hd)->next)
+	{
+		update_list(hd, &(*hd)->next, list_line_cmds(NULL, *line + *i + 1, 1));
+		(*hd) = (*hd)->next;
+	}
+	else
+		add_front(hd, list_line_cmds(NULL, *line + *i + 1, 1));
+}
+
+void	parse_redir(char **line, t_cmd_list **hd ,int *i)
+{
+	if ((*line)[*i] == '<')
+		(*hd)->redir = '<';
+	else if ((*line)[*i] == '>')
+	{
+		if ((*line)[*i + 1] == '>')
+		{
+			(*hd)->redir = 'a';
+			(*i)++;
+		}
+		else
+			(*hd)->redir = '>';
+	}
+}
 
 void	parse_list_line(char **line, t_cmd_list *list, t_cmds *cmds)
 {	
@@ -24,33 +53,17 @@ void	parse_list_line(char **line, t_cmd_list *list, t_cmds *cmds)
 	i = -1;
 	while (++i <= len)
 	{
-		if ((*line)[i] == ';' || (*line)[i] == '\0')
+		if (((*line)[i] == ';' || (*line)[i] == '\0') && (hd->end = 1))
 		{
 			((hd->prev && hd->prev->end) || !hd->prev) ? hd->start = 1 : 0;
-			hd->end = 1;
 			if ((*line)[i] == ';')
 				break ;
 		}
 		else if ((*line)[i] == '|' && (*line)[i - 1] != '\\')
-		{
-			(!hd->prev || (hd->prev && hd->prev->end)) ? hd->start = 1 : 0;
-			(*line)[i] = '\0';
-			if (hd->next)
-			{
-				update_list(&hd, &hd->next, list_line_cmds(NULL, *line + i + 1, 1));
-				hd = hd->next;
-			}
-			else
-				add_front(&hd, list_line_cmds(NULL, *line + i + 1, 1));
-		}
-		else if ((*line)[i] == '<')
-			hd->redir = '<';
-		else if ((*line)[i] == '>')
-		{
-			if ((*line)[i + 1] == '>' && (i++))
-				hd->redir = 'a';
-			else
-				hd->redir = '>';
-		}
+			parse_pipe(line, &hd, &i);
+		else if ((*line)[i] == '<' || (*line)[i] == '>')
+			parse_redir(line, &hd, &i);
 	}
+
+	// create a fct to separate the redirection (ad sp before & after it)
 }
