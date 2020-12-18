e17436f7a7d05d78d3b1dab1071c89c95a864bd2
Imane Chejra <ichejra@e2r7p6.1337.ma>
2020-12-18 14:21:18 +0100

    update libft && gitignore

---
+++ libft/ft_atoi_l.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_l.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichejra <ichejra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/18 12:54:01 by ichejra           #+#    #+#             */
+/*   Updated: 2020/12/18 12:54:34 by ichejra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static long	ft_check_long(long n, int sign)
+{
+	if (n < 0 && sign > 0)
+		return (-1);
+	if (n > 0 && sign < 0)
+		return (0);
+	return (n);
+}
+
+long	ft_atoi_l(const char *str)
+{
+	int			i;
+	long long	n;
+	int			sign;
+
+	i = 0;
+	n = 0;
+	sign = 1;
+	while (str[i] == '\t' || str[i] == ' ' || str[i] == '\n' ||
+		str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - 48);
+		i++;
+	}
+	n = n * sign;
+	return (ft_check_long(n, sign));
+}
